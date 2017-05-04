1f262fc628f610104c1db65b48bd796a28756c23
arturgudiev <arturgudiev93@gmail.com>
2017-05-04 08:23:22 +0300

    finish first home work

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Artur on 27.04.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+//        WebDriver driver = new FirefoxDriver();
+//        driver.navigate().to("https://www.epam.com");
+
+
+    }
+}
